80c4aae2bd1a26936543b354e0803c95cf6c9e53
Sacred Duckwhale <duckwhale@mail.ru>
2017-09-11 23:00:22 +0200

    Added TaskDB:GetNumTasks() function

--- Core/TaskDB.lua
+++ Core/TaskDB.lua
@@ -38,6 +38,28 @@ local function Print()
 	
 end
 
+
+--- Returns the number of (non-default) tasks contained in the TaskDB
+-- @param[opt] countDefaults Count the default tasks, too (which use String keys and not integers)
+-- @return the number of (non-default) tasks
+local function GetNumTasks(countDefaults)
+
+	local numCustomTasks = #AM.db.global.tasks
+	
+	-- Only custom tasks -> easy peasy
+	if not countDefaults then return numCustomTasks end
+	
+	-- Count default Tasks, also -> It's not difficult but does require some extra effort
+	local defaultTasks = AM.TaskDB:GetDefaultTasks()
+	local numDefaultTasks = 0 -- Can't get them as easily, since they don't use integer keys
+	for k, v in pairs(defaultTasks)
+		numDefaultTasks = numDefaultTasks + 1
+	end
+	
+	return (numCustomTasks + numDefaultTasks)
+
+end
+
 --- Creates a new Task object with the given name and returns a reference to it
 -- The name can be used as its key in the TaskDB later, although it's not mandatory to honour that convention
 -- @return A reference to the newly-created Task object
@@ -63,6 +85,7 @@ TaskDB = {
 
 	Print = Print,
 	CreateTask = CreateTask,
+	GetNumTasks = GetNumTasks,
 	
 }
 
