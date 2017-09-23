9f4a5da8849e3204edebeade93650420240323e1
Sacred Duckwhale <duckwhale@mail.ru>
2017-09-23 21:00:50 +0200

    EventHandler: Added stub

---
+++ Core/EventHandler.lua
@@ -0,0 +1,34 @@
+  ----------------------------------------------------------------------------------------------------------------------
+    -- This program is free software: you can redistribute it and/or modify
+    -- it under the terms of the GNU General Public License as published by
+    -- the Free Software Foundation, either version 3 of the License, or
+    -- (at your option) any later version.
+	
+    -- This program is distributed in the hope that it will be useful,
+    -- but WITHOUT ANY WARRANTY; without even the implied warranty of
+    -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    -- GNU General Public License for more details.
+
+    -- You should have received a copy of the GNU General Public License
+    -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+----------------------------------------------------------------------------------------------------------------------
+
+--- Designed to handle interaction with the player, react to their input, and adjust program behaviour accordingly
+-- @module Controllers
+
+--- EventHandlers.lua.
+-- Provides a simple interface to toggle specific categories of event triggers and react to them according to the addon's needs. Only events that are caused by some player action are covered here.
+-- @section GUI
+
+
+local addonName, AM = ...
+if not AM then return end
+
+
+
+
+local EventHandler = {
+
+}
+
+return EventHandler
