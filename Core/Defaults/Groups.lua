9684c4045f131008fe1994b87d42c13187f28225
Sacred Duckwhale <duckwhale@mail.ru>
2017-10-08 20:45:54 +0200

    GUI: Added automatic generation (and ordering) of default groups

--- Core/Defaults/Groups.lua
+++ Core/Defaults/Groups.lua
@@ -131,8 +131,6 @@ local defaultGroups = { -- TODO: Generate automatically from import table (only 
 --			"DAILY_WOD_PETBATTLE_ERRIS",
 --	TODO: EVENT garrison table		"DAILY_WOD_GARRISONMISSIONS",
 --	TODO. Zone/Entered		"DAILY_WOD_HERBGARDEN",
--- TODO: Objectives(*)		
-	
 		"UNLOCK_LEGION_KOSUMOTH",
 		"WQ_LEGION_KOSUMOTH",
 		
@@ -246,6 +244,8 @@ local defaultGroups = { -- TODO: Generate automatically from import table (only 
 			"LEGION_DAILY_RITUALOFDOOM",
 			"LEGION_DAILY_TWISTINGNETHER",
 
+			"MILESTONE_LEGION_IMPROVINGONHISTORY",
+			"MILESTONE_LEGION_THEMOTHERLODE",
 			
 			-- Miscellaneous stuff
 			"WOTLK_MYSTERIOUSEGG",
@@ -253,24 +253,122 @@ local defaultGroups = { -- TODO: Generate automatically from import table (only 
 		},
 	},
 
-	MILESTONES = {
-		name = "Milestones",
-		iconPath = "achievement_garrison_tier03_alliance",
-		taskList = {
-		
-			"MILESTONE_LEGION_IMPROVINGONHISTORY",
-			"MILESTONE_LEGION_THEMOTHERLODE",
-			
-		},
-	},
 }
 
+-- These lists will be generated from the default list
+-- For this to work, ALL default tasks (and milestones) must adhere to the naming scheme:
+-- TASKTYPE OR MILESTONE .. _ .. EXPANSIONSHORT .. _ .. NAME
+-- where EXPANSIONSHORT is one of
+-- CLASSIC, TBC; WOTLK, CATA, MOP, WOD; LEGION
+-- and TASKTYPE can be anything, such as DAILY, WEEKLY, WORLDEVENT, ...
+local TASKS = {
+	name = "Tasks",
+	iconpath = "inv_misc_book_07",
+	taskList = {},
+}
+
+local MILESTONES = {
+	name = "Milestones",
+	iconPath = "achievement_zone_valeofeternalblossoms_loremaster", --"achievement_garrison_tier03_alliance",
+	taskList = {},
+}
+
+local expansions = {
+
+	CLASSIC = {
+		name = "Classic",
+		iconPath = "expansionicon_classic",
+		taskList = {},
+	},
+	
+	TBC = {
+		name = "The Burning Crusade",
+		iconPath = "expansionicon_theburningcrusade",
+		taskList = {},
+	},
+	
+	WOTLK = {
+		name = "Wrath of the Lich King",
+		iconPath = "expansionicon_wrathofthelichking",
+		taskList = {},
+	},
+	
+	CATA = {
+		name = "Cataclysm",
+		iconPath = "expansionicon_cataclysm",
+		taskList = {},
+	},
+	
+	MOP = {
+		name = "Mists of Pandaria",
+		iconPath = "expansionicon_mistsofpandaria",	--inv_pandarenserpentmount		pandarenracial_innerpeace
+		taskList = {},
+	},
+	
+	WOD = {
+		name = "Warlords of Draenor",
+		iconPath = "inv_fellessergronnmount",
+		taskList = {},
+	},
+	
+	LEGION = {
+		name = "Legion",
+		iconPath = "inv_legionadventure",
+		taskList = {},
+	},
+
+}
+
+-- Manual order to make sure they are displayed properly
+local order = {
+	ALL_THE_TASKS = 1,
+	ALL_TASKS = 1, -- TODO?
+	MILESTONES = 2,
+	TASKS = 3,
+	LEGION = 4,
+	WOD = 5,
+	MOP = 6,
+	CATA = 7,
+	WOTLK = 8,
+	TBC = 9,
+	CLASSIC = 10,
+}
+
+-- Filter default group to categorize tasks
+local strfind = string.find
+for index, taskName in pairs(defaultGroups.ALL_THE_TASKS["taskList"]) do -- Check each Task or Milestone and copy it to the list
+	
+	if strfind(taskName, "TASK") ~= nil then -- Add to generic Tasks list
+		TASKS["taskList"][#TASKS+1] = taskName
+	end
+	
+	if strfind(taskName, "MILESTONE") ~= nil then	-- Add to generic Milestones list
+		MILESTONES["taskList"][#MILESTONES+1] = taskName
+	end
+	
+	for expansionShort, group in pairs(expansions) do	-- Fill one list for each expansion
+		if strfind(taskName, expansionShort) ~= nil then
+			group["taskList"][#group+1] = taskName
+		end
+	end
+	
+end
+
+-- Add the categorized tasks to their separate lists		
+for expansionShort, data in pairs(expansions) do -- Add the auto-generated default groups to the defaults list
+	defaultGroups[expansionShort] = data
+end
+
+defaultGroups.TASKS = TASKS
+defaultGroups.MILESTONES = MILESTONES
+
+gt123 = defaultGroups -- TODO
 
 --- Return the table containing default Group entries
 local function GetDefaultGroups()
 	
 	local defaults = {}
-	
+
 	for key, entry in pairs(defaultGroups) do -- Create Group object and store it
 
 		-- Add values
@@ -285,9 +383,9 @@ local function GetDefaultGroups()
 		-- Store in table that will be added to AceDB defaults
 --		AM:Debug("Loaded default Group with key = " .. tostring(key) .. ", tostring() = " .. tostring(Group), "GroupDB")
 		defaults[key] = Group
-		
+
 	end
-	
+
 	return defaults
 
 end
@@ -300,10 +398,10 @@ local function GetOrderedDefaultGroups()
 	-- Build table of keys
 	local keys = {}
 	for key, value in pairs(defaults) do 
-		keys[#keys+1] = key
+		keys[order[key]] = key -- Reorders each group according to its entry in the order LUT -> ALL_TASKS = 1, MILESTONES = 2, TASKS = 3, ... and then the expansions
 	end
 	
-	table.sort(keys)
+	--table.sort(keys)
 	
 	local orderedDefaults = {}
 	for key, value in ipairs(keys) do -- Add the groups in alphabetical order (value is actually the group's key/ID)
