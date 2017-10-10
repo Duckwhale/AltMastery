6a8c013e05501a555ce230823a01e1b9346ef6ad
Sacred Duckwhale <duckwhale@mail.ru>
2017-10-10 12:38:06 +0200

    Tasks: Added monthly reward quest for The Consortium (TBC faction)

--- Core/Defaults/Tasks.lua
+++ Core/Defaults/Tasks.lua
@@ -902,6 +902,21 @@ local defaultTasks = {
 		
 	},
 	
+	MONTHLY_TBC_MEMBERSHIPBENEFITS = {
+		name = "Membership Benefits",
+		description = "Receive a Premium Bag of Gems from Gezhe in Nagrand (Outland)", -- TODO: Only Premium if reputation is exalted - what about the others?
+		notes = "Gems",
+		iconPath = "inv_misc_bag_17",
+		Criteria = "NumObjectives(\"MONTHLY_TBC_MEMBERSHIPBENEFITS\") > 0",
+		Filter = "Level() < 70",
+		Objectives = {
+			"Quest(9886) AS Membership Benefits (Friendly)",
+			"Quest(9884) AS Membership Benefits (Honored)",
+			"Quest(9885) AS Membership Benefits (Revered)",
+			"Quest(9887) AS Membership Benefits (Exalted)",
+		},
+	},
+	
 	MILESTONE_LEGION_THEMOTHERLODE = {
 		name = "The Motherlode",
 		description = "TODO",
