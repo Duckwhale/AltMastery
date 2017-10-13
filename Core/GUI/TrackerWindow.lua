cd8f0f6f6b248c0d710ba0d8e91a4b6b6e65d048
Sacred Duckwhale <duckwhale@mail.ru>
2017-10-13 21:48:48 +0200

    Cleanup

--- Core/GUI/TrackerWindow.lua
+++ Core/GUI/TrackerWindow.lua
@@ -25,29 +25,11 @@ if not AM then return end
 --- Show main window for the Tracker
 -- @param self ...
 local function Show(self)
-
-	-- TODO: Release children back into the widget pool ? Not doing this here because most children are fairly static and should not have to be re-aquired when showing the main display. Instead, the TrackerContent container should release its children, which are the dynamically created Groups and Tasks (that are the main reason for using AceGUI's frame pool in the first place)
-
-	local AceGUI = LibStub("AceGUI-3.0")
+	
 	assert(AceGUI, "AceGUI not found")
 	
 	if not self.frame then -- First call -> Create new instance via AceGUI and configure it
 	
-		self.frame = AceGUI:Create("AMWindow")
-		self.frame:SetLayout("AMRows")
-		
-		-- Add logo image (TODO: Not very interactive yet)
-		local logoSpecs = {
-			
-			type = "Logo",
-			parent = self.frame.frame,
-			size = { 132, 60 }
-			
-		}
-		local InteractiveLogo = AM.GUI:BuildFrame(logoSpecs)
-		InteractiveLogo:SetPoint("TOPLEFT", self.frame.frame, "TOPLEFT", 0, 60)
-		
-		-- Retrieve style for the content panes
 		local activeStyle = AM.GUI:GetActiveStyle()
 		local contentPaneStyle = activeStyle.frameColours.ContentPane
 	
@@ -70,26 +52,14 @@ local function Show(self)
 		-- Add container for the group selection icons
 		local GroupSelectionPane = AceGUI:Create("InlineGroup") -- TODO: Use same type as content panes?
 		border = GroupSelectionPane.content:GetParent()
-		
-		local titletext = GroupSelectionPane.titletext
-		titletext:SetPoint("TOPLEFT", 10, 0)
-		titletext:SetPoint("TOPRIGHT", -10, 0)
-		titletext:SetJustifyH("CENTER")
-		titletext:SetHeight(24)
-		--titletext:Hide() -- TODO: Needs to be implemented properly so it fits with the visual style of the rest
-	--	titletext:SetSize(activeStyle.fontSizes.normal)
-		
 		border:ClearAllPoints()
 		local padding = AM.db.profile.settings.display.contentPadding
 		border:SetPoint("TOPLEFT", padding, 0) -- TODO: What's with the offset? I think it's for the title, but why can't it just work normally?
 		border:SetPoint("BOTTOMRIGHT", -padding, 36)
---		GroupSelectionPane:SetAutoAdjustHeight(false)
 	
 		AM.GUI:SetFrameColour(border, activeStyle.frameColours.GroupSelectionPane)
 		local r, g, b = AM.Utils.HexToRGB(activeStyle.frameColours.GroupSelectionPane.border, 255)
 		border:SetBackdropBorderColor(r, g, b, activeStyle.frameColours.GroupSelectionPane.borderAlpha) -- This should be updated dynamically (TODO)
---		GroupSelectionPane:SetTitle("GROUPS")
---		GroupSelectionPane:SetFullHeight(true)
 		GroupSelectionPane:SetRelativeWidth(1)
 		GroupSelectionPane:SetLayout("List")
 		RightPane:AddChild(GroupSelectionPane)
@@ -102,13 +72,10 @@ local function Show(self)
 		border:ClearAllPoints()
 		border:SetPoint("TOPLEFT", padding, -padding)
 		border:SetPoint("BOTTOMRIGHT", -padding, 36)
-	--	TrackerPane:SetAutoAdjustHeight(false)
 	
 		AM.GUI:SetFrameColour(border, activeStyle.frameColours.TrackerPane)
 		local r, g, b = AM.Utils.HexToRGB(activeStyle.frameColours.TrackerPane.border, 255)
 		border:SetBackdropBorderColor(r, g, b, activeStyle.frameColours.TrackerPane.borderAlpha) -- This should be updated dynamically (TODO)
-	--	TrackerPane:SetTitle("Tasks")
-	--	TrackerPane:SetFullHeight(true)
 		TrackerPane:SetRelativeWidth(1)
 		TrackerPane:SetLayout("List")
 		LeftPane:AddChild(TrackerPane)
@@ -117,14 +84,10 @@ local function Show(self)
 		AM.TrackerPane.widget = TrackerPane
 		
 	end
+
 	-- TODO: Global GUI:Update() function
 	AM.GroupSelector:Update()
-
 	AM.TrackerPane:Update()
---	AM.TrackerPane:ReleaseWidgets() -- TODO: Combine this in Update() and call that instead?
-	--AM.TrackerPane:UpdateGroups() -- Update tracker to display all Tasks and nested Groups for the currently active Group
-	
-	-- Show the frame
 	self.frame:Show()
 
 end
